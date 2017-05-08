951e2fca33c452d378c83ab53045c70da79db23a
Octavio Martin <octavio.martin.montenegro@teamaol.com>
2017-05-08 20:05:51 +0100

    Added logging capabilities

--- http-sinkhole.go
+++ http-sinkhole.go
@@ -2,14 +2,24 @@ package main
 
 import (
 	"fmt"
+	"gopkg.in/natefinch/lumberjack.v2"
+	"log"
 	"net/http"
+	"os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusGone)
+	log.Printf("Referer: %s\n", r.Header.Get("Referer"))
 }
 
 func main() {
+	log.SetOutput(&lumberjack.Logger{
+		Filename:   os.Args[1],
+		MaxSize:    250, // megabytes
+		MaxBackups: 3,
+		MaxAge:     1, //days
+	})
 	fmt.Printf("Starting http-sinkhole at 8080 port")
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
