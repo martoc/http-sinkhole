556269c481f7aab2526e075de553b6bdb043913d
Octavio Martin <octavio.martin.montenegro@teamaol.com>
2017-05-09 12:11:11 +0100

    Added healtchcheck

--- http-sinkhole.go
+++ http-sinkhole.go
@@ -13,6 +13,10 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Referer: %s\n", r.Header.Get("Referer"))
 }
 
+func HealthHeckHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
+
 func main() {
 	log.SetOutput(&lumberjack.Logger{
 		Filename:   os.Args[1],
@@ -21,6 +25,7 @@ func main() {
 		MaxAge:     1, //days
 	})
 	fmt.Printf("Starting http-sinkhole at 8080 port")
+	http.HandleFunc("/_healthcheck_", HealthHeckHandler)
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
 }
